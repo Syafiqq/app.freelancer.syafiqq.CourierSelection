a2d2aa2af997e405719a4e23766d52cc7369a836
Syafiqq <syafiq.rezpector@gmail.com>
2017-04-24 22:38:13 +0700

    Plant timber

--- app/src/main/java/app/freelancer/syafiqq/courierselection/CourierSelectionMain.java
+++ app/src/main/java/app/freelancer/syafiqq/courierselection/CourierSelectionMain.java
@@ -3,6 +3,8 @@ package app.freelancer.syafiqq.courierselection;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import timber.log.Timber;
+
 public class CourierSelectionMain extends AppCompatActivity
 {
 
@@ -11,5 +13,17 @@ public class CourierSelectionMain extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_courier_selection_main);
+
+        this.initializeTimber();
+
+        Timber.i("Best Alternative %s", "Test");
+    }
+
+    private void initializeTimber()
+    {
+        if(BuildConfig.DEBUG)
+        {
+            Timber.plant(new Timber.DebugTree());
+        }
     }
 }

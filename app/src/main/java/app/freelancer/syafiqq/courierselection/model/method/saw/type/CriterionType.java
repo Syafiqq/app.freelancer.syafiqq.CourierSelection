51b8fbc2cd72e7a250c1c98453f881fbc2682890
Syafiqq <syafiq.rezpector@gmail.com>
2017-04-24 22:40:44 +0700

    SAW : add criterion type

---
+++ app/src/main/java/app/freelancer/syafiqq/courierselection/model/method/saw/type/CriterionType.java
@@ -0,0 +1,15 @@
+package app.freelancer.syafiqq.courierselection.model.method.saw.type;
+
+/**
+ * This <CourierSelection> project created by :
+ * Name         : syafiq
+ * Date / Time  : 24 April 2017, 8:40 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+public enum CriterionType
+{
+    BENEFIT,
+    COST
+}

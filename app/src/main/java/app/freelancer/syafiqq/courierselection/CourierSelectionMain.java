93bac5a6f33a3572c2a5a14e7cc358d5465cf5b3
Syafiqq <syafiq.rezpector@gmail.com>
2017-04-24 22:35:32 +0700

    Initial Commit

---
+++ app/src/main/java/app/freelancer/syafiqq/courierselection/CourierSelectionMain.java
@@ -0,0 +1,15 @@
+package app.freelancer.syafiqq.courierselection;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CourierSelectionMain extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_courier_selection_main);
+    }
+}

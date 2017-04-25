bdf79c0bb4b418be4d98c27c4c07b603b5a072c2
Syafiqq <syafiq.rezpector@gmail.com>
2017-04-25 09:27:11 +0700

    TOPSIS : add criterion type

---
+++ app/src/main/java/app/freelancer/syafiqq/courierselection/model/method/topsis/type/CriterionType.java
@@ -0,0 +1,15 @@
+package app.freelancer.syafiqq.courierselection.model.method.topsis.type;
+
+/**
+ * This <CourierSelection> project created by :
+ * Name         : syafiq
+ * Date / Time  : 25 April 2017, 7:47 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+public enum CriterionType
+{
+    BENEFIT,
+    COST,
+}

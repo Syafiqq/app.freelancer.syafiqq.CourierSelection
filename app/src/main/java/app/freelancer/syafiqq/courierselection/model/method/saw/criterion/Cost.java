a304b2a5e24f47fa1b174b490fdd2ca4bbace5df
Syafiqq <syafiq.rezpector@gmail.com>
2017-04-30 19:31:44 +0700

    Add min max criterion

--- app/src/main/java/app/freelancer/syafiqq/courierselection/model/method/saw/criterion/Cost.java
+++ app/src/main/java/app/freelancer/syafiqq/courierselection/model/method/saw/criterion/Cost.java
@@ -24,6 +24,8 @@ import app.freelancer.syafiqq.madm.saw.core.factory.Weight;
 public class Cost extends Criterion
 {
     public static final CriterionType TYPE = CriterionType.BENEFIT;
+    public static final int           min  = 1;
+    public static final int           max  = 3;
 
     private int    value;
     private double normalization;
--- app/src/main/java/app/freelancer/syafiqq/courierselection/model/method/saw/criterion/Packaging.java
+++ app/src/main/java/app/freelancer/syafiqq/courierselection/model/method/saw/criterion/Packaging.java
@@ -24,6 +24,8 @@ import app.freelancer.syafiqq.madm.saw.core.factory.Weight;
 public class Packaging extends Criterion
 {
     public static final CriterionType TYPE = CriterionType.BENEFIT;
+    public static final int           min  = 1;
+    public static final int           max  = 4;
 
     private int    value;
     private double normalization;
